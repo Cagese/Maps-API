9b0062f241a08d45df8dccc9759c6ca3eb8d8638
Cagese <mikhail_morozov_2006@mail.ru>
2024-02-02 17:17:10 +0300

    init commit

--- main.py
+++ main.py
@@ -15,12 +15,10 @@ class Example(QWidget):
         self.initUI()
 
     def getImage(self):
-        map_request = "http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&spn=0.002,0.002&l=map"
-        response = requests.get(map_request)
+        response = requests.get(search_api_server,params=map_params)
 
         if not response:
             print("Ошибка выполнения запроса:")
-            print(map_request)
             print("Http статус:", response.status_code, "(", response.reason, ")")
             sys.exit(1)
 
@@ -46,6 +44,15 @@ class Example(QWidget):
 
 
 if __name__ == '__main__':
+    search_api_server = 'http://static-maps.yandex.ru/1.x/'
+    address_ll = '37.530887,55.703118'
+    delta = '0.002'
+    map_params = {
+        "ll": address_ll,
+        "spn": ",".join([delta, delta]),
+        "l": "map",
+    }
+
     app = QApplication(sys.argv)
     ex = Example()
     ex.show()
